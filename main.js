c9bb5413ac411a430e36768a8ddbfc67ebcf55d6
Jacey Davis <jaceyd923@gmail.com>
2023-09-02 18:01:29 -0600

    added data model

---
+++ main.js
@@ -0,0 +1,23 @@
+let todos = [
+    {
+        id: 1,
+        name: "do homework",
+        isComplete: false,
+        category: "school",
+        dueDate: "2023-09-23"
+    },
+    {
+        id: 2,
+        name: "submit report",
+        isComplete: false,
+        category: "work",
+        dueDate: "2023-09-04"
+    },
+    {
+        id: 3,
+        name: "go grocery shopping",
+        isComplete: true,
+        category: "home",
+        dueDate: "none"
+    }
+]
